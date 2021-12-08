1cca21d5bc507804d0755623a1d627e65ead897b
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-12-08 17:14:16 +0900

    Support Podman

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ set -eu -o pipefail
 
 IMAGE_NAME='shakiyam/pip-tools'
 readonly IMAGE_NAME
-CURRENT_IMAGE="$(docker image ls -q $IMAGE_NAME:latest)"
+DOCKER=$(command -v podman || command -v docker)
+readonly DOCKER
+CURRENT_IMAGE="$($DOCKER image ls -q $IMAGE_NAME:latest)"
 readonly CURRENT_IMAGE
-docker image build \
-  -t "$IMAGE_NAME" \
-  "$(dirname "$0")"
-LATEST_IMAGE="$(docker image ls -q $IMAGE_NAME:latest)"
+$DOCKER image build -t "$IMAGE_NAME" "$(dirname "$0")"
+LATEST_IMAGE="$($DOCKER image ls -q $IMAGE_NAME:latest)"
 readonly LATEST_IMAGE
 if [[ "$CURRENT_IMAGE" != "$LATEST_IMAGE" ]]; then
-  docker image tag $IMAGE_NAME:latest $IMAGE_NAME:"$(date +%Y%m%d%H%S)"
+  $DOCKER image tag $IMAGE_NAME:latest $IMAGE_NAME:"$(date +%Y%m%d%H%S)"
 fi
